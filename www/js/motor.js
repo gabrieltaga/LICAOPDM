f4f4cf4d240de98666ba01208b5ca7796f972847
gabrieltaga <gabrieltaga00@gmail.com>
2023-06-02 21:14:10 +0900

    Project's Initial Commit

---
+++ www/js/motor.js
@@ -0,0 +1,30 @@
+var nome
+var selecao
+var selecaodisciplina 
+var selecaomencao
+
+function cadastrar() {
+    nome = document.getElementById('inputnome').value
+    document.getElementById('nomealuno').textContent = nome
+    
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    selecao = document.getElementById('mencao')
+    selecaomencao = selecao.options [selecao.selectedIndex].value
+    
+    localStorage.setItem(selecaodisciplina, selecaomencao)
+
+}
+
+function listar(){
+    var exibirmencao
+
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectedIndex].value
+
+    localStorage.getItem(selecaodisciplina)
+
+    document.getElementById('exibirdisciplina').textContent = selecaodisciplina
+    document.getElementById('exibirmencao').textContent = exibirmencao
+}
